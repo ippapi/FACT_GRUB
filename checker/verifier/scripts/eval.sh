3b3a666eae95e3117de6e343f82eb95069cfeb08
ippapi <coricutiphong@gmail.com>
2025-09-27 21:33:57 +0700

    changing eval code

--- checker/verifier/scripts/eval.sh
+++ checker/verifier/scripts/eval.sh
@@ -2,7 +2,7 @@ DATA_NAME=./checker/verifier/dummy
 DATA_DIR=./$DATA_NAME
 MODEL_PATH = $1
 
-python  ../main.py \
+python ./checker/verifier/main.py \
     --dev_file $DATA_DIR/dev.parquet \
     --model_name MODEL_PATH \
     --resume \
