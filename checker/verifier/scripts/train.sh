0cc71b6f50500eee08f86cd70f85895a62645605
ippapi <coricutiphong@gmail.com>
2025-09-28 09:11:08 +0700

    oldest trick in the book

--- checker/verifier/scripts/train.sh
+++ checker/verifier/scripts/train.sh
@@ -7,7 +7,7 @@ python ./checker/verifier/main.py  \
     --data_name $DATA_NAME \
     --train_file $DATA_DIR/train.parquet \
     --dev_file $DATA_DIR/dev.parquet \
-    --model_name "vinai/phobert-basevinai/phobert-large" \
+    --model_name "vinai/phobert-large" \
     --epochs 50 \
     --batch_size 32 \
     --lr 2e-05 \
