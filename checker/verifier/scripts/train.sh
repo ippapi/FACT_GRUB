1f36dcf3384da1ad31098878091d84a1ac512158
ippapi <coricutiphong@gmail.com>
2025-10-01 13:33:21 +0700

    change model

--- checker/verifier/scripts/train.sh
+++ checker/verifier/scripts/train.sh
@@ -7,8 +7,8 @@ python ./checker/verifier/main.py  \
     --data_name $DATA_NAME \
     --train_file $DATA_DIR/train.parquet \
     --dev_file $DATA_DIR/dev.parquet \
-    --model_name "vinai/phobert-base" \
-    --epochs 30 \
+    --model_name "VietAI/vit5-base" \
+    --epochs 5 \
     --batch_size 32 \
     --lr 2e-05 \
     --logging_steps 10 \
