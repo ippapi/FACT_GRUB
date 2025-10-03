e6027016b9ec2a84ad0f34f1f834f104a61a9a71
ippapi <coricutiphong@gmail.com>
2025-10-03 09:53:32 +0700

    repo khổ nhất thế giới

--- checker/verifier/scripts/eval.sh
+++ checker/verifier/scripts/eval.sh
@@ -5,7 +5,7 @@ MODEL_PATH=$1
 export PYTHONPATH=$(pwd)
 
 python ./checker/verifier/main.py \
-    --dev_file $DATA_DIR/dev.parquet \
+    --dev_file $DATA_DIR/test.parquet \
     --model_name $MODEL_PATH \
     --resume \
     --do_eval
